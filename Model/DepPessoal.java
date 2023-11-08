3541e8908d05effb6b8af9e9df6627171c7ee9ac
joellmirr <149622584+joellmirr@users.noreply.github.com>
2023-11-08 09:29:50 -0300

    update

--- Model/DepPessoal.java
+++ Model/DepPessoal.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.projetoaula753.Turma753Senac.Model;
+package com.mycompany.Turma753Senac.Model;
 
 /**
  *
  * @author joelmir
  */
 public class DepPessoal extends Empresa {
-    Funcionario Funcionario = new Funcionario();
+   
 }
