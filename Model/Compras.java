5c620d08eeb7953b71d1b11b74a62c49ad4b6ad9
joellmirr <149622584+joellmirr@users.noreply.github.com>
2023-11-09 09:17:44 -0300

    up

--- Model/Compras.java
+++ Model/Compras.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.projetoaula753.Turma753Senac.Model;
+package com.mycompany.Turma753Senac.Model;
 
 /**
  *
