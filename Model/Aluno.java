975d3f9c8d1360479f78c28d68d33205a15b04ce
joellmirr <149622584+joellmirr@users.noreply.github.com>
2023-11-14 21:45:12 -0300

    add

--- Model/Aluno.java
+++ Model/Aluno.java
@@ -10,8 +10,8 @@ package com.mycompany.Turma753Senac.Model;
  */
 public class Aluno {
     String Nome;
-    int nota1;
-    int nota2;
+    float nota1;
+    float nota2;
     float media;
     String status;
 
@@ -23,21 +23,22 @@ public class Aluno {
         this.Nome = Nome;
     }
 
-    public int getNota1() {
+    public float getNota1() {
         return nota1;
     }
 
-    public void setNota1(int nota1) {
+    public void setNota1(float nota1) {
         this.nota1 = nota1;
     }
 
-    public int getNota2() {
+    public float getNota2() {
         return nota2;
     }
 
-    public void setNota2(int nota2) {
+    public void setNota2(float nota2) {
         this.nota2 = nota2;
     }
+    
 
     public float getMedia() {
         media=(this.nota1+this.nota2)/2;
